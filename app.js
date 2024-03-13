69fce8844414ed3de83cb4669867d30e3217f53d
waltscode <walterswork91@outlook.com>
2024-03-13 16:56:37 -0400

    added simple transition functionality

---
+++ app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(element => {
+  observer.observe(element);
+});
